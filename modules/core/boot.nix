8b859b7deb60c6fd4c710070c213447000cbb40e
TriplePointCat <imoen.hawthorne@gmail.com>
2025-11-11 18:14:32 -0500

    add ntfs support

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -8,7 +8,7 @@
     kernelModules = ["v4l2loopback"];
     extraModulePackages = [config.boot.kernelPackages.v4l2loopback];
     kernel.sysctl = {"vm.max_map_count" = 2147483642;};
-
+    supportedFilesystems = ["ntfs"];
     # loader.systemd-boot.enable = true;
     # loader.efi.canTouchEfiVariables = true;
     loader = {
