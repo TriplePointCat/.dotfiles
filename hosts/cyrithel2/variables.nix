da6d669c6519134e35ac1b0e726806265dc0f463
TriplePointCat <68162904+TriplePointCat@users.noreply.github.com>
2026-06-27 13:12:24 -0400

    add cyrithel2

---
+++ hosts/cyrithel2/variables.nix
@@ -0,0 +1,48 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "TriplePointCat";
+  gitEmail = "68162904+TriplePointCat@users.noreply.github.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "
+    monitor = eDP-1,1920x1080@144,auto,1
+    monitor = HDMI-A-1,highres, auto,1
+  ";
+  extraHardwareSettings = "";
+  defaultWallpaper = "MST23.png";
+
+  theme = "catppuccin-mocha";
+
+  fontSizes = {
+    applications = 14;
+    terminal = 18;
+    desktop = 12;
+    popups = 12;
+  };
+
+  # Waybar Settings
+  clock24h = true;
+
+  # variables which toggle packages
+  gamedev = false;
+  gaming = true;
+  texlive = false;
+  silly = false;
+
+  # Program Options
+  browser = "zen"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "";
+  consoleKeyMap = "us";
+
+  editor = "nvim";
+  EDITOR = "nvim";
+  VISUAL = "nvim";
+
+  # For Nvidia Prime support
+  intelID = "PCI:1:0:0";
+  nvidiaID = "PCI:0:2:0";
+
+  # Enable NFS
+  enableNFS = true;
+}
